044022f0d3401043f6a04f13a01166f9aade320c
monkey dev <monkey.dev.1995@gmail.com>
2023-09-03 22:58:40 -0500

    change env variable v2

--- src/app/constants/contract.ts
+++ src/app/constants/contract.ts
@@ -271,9 +271,9 @@ export const betAmountList = [
 
 
 export const getBetAmountList = () => {
-	if(process.env.CHAIN_ENV === "development") {
+	if(process.env.NEXT_PUBLIC_CHAIN_ENV === "development") {
     return betAmountListDev;
-  } else if(process.env.CHAIN_ENV === "production") {
+  } else if(process.env.NEXT_PUBLIC_CHAIN_ENV === "production") {
     return betAmountList
   } else return betAmountList
 }
